45b9b5e1c00404b4ac05d799a3e60538f29b14b8
Naix <76706715+NaixYZ@users.noreply.github.com>
2022-02-21 20:08:06 +0100

    Add files via upload

---
+++ clean.bat
@@ -0,0 +1,22 @@
+@echo off
+echo Welcome to Pc Cleaner
+echo Cleaning temporary files
+color D
+del /s /f /q %SYSTEMDRIVE%\windows\temp\*.*
+rd /s /q %SYSTEMDRIVE%\windows\temp
+md c:\windows\temp
+del /s /f /q %SYSTEMDRIVE%\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+echo Done cleaning temporary files
+echo Cleaning logs
+md %temp%
+del /q /f /s %SYSTEMDRIVE%\Temp\*.*
+del /q /f /s %WINDIR%\Prefetch\*.*
+del /q /f /s %SYSTEMDRIVE%\*.log
+del /q /f /s %SYSTEMDRIVE%\*.bak
+del /q /f /s %SYSTEMDRIVE%\*.gid
+echo Cleaned logs
+echo Thanks for using Pc Cleaner
+pause
+cls
